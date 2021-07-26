79d8c12948f2c7543fe4a5c42d2df7bf1919f3e4
Solo Scribble Creatives <34957574+soloscribblecreatives@users.noreply.github.com>
2021-07-26 18:35:14 +0530

    Ajaduo

--- global.js
+++ global.js
@@ -385,28 +385,28 @@ currentSlide();
 var selectedContentPath='';
 switch(pg_id){
 	case 1:
-	content='<link rel="stylesheet" type="text/css" href="slide1/slide1.css" media="screen"/><div class="s1_1"><img src="slide1/s1_1.png"/></div>';
+	content='<link rel="stylesheet" type="text/css" href="slide1/slide1.css" media="screen"/><div class="background"><img src="slide1/s1_1.jpg" width="1024" height="768" alt=""></div><div class="logo"><img src="slide1/s1_2.png" alt=""></div>';
 	break;
 	case 2:
-	content='<link rel="stylesheet" type="text/css" href="slide2/slide2.css" media="screen"/><div class="s2_1"><img src="slide2/s2_1.png"/></div><div class="s2_2"><img src="slide2/s2_2.png"/></div><div class="s2_3"><img src="slide2/s2_3.png"/></div><div class="s2_4"><img src="slide2/s2_4.png"/></div><div class="s2_5"><img src="slide2/s2_5.png"/></div><div class="s2_6"><img src="slide2/s2_6.png"/></div><div class="s2_pop1" onclick="s2_pop1()"></div><div class="s2_c1ose1" onclick="s2_close1()"></div>';
+	content='<link rel="stylesheet" type="text/css" href="slide2/slide2.css" media="screen"/><div class="background"><img src="slide2/s2_1.jpg" width="1024" height="768" alt=""></div><div class="slider"><img src="slide2/s2_2.png" alt=""></div><div class="logo"><img src="slide2/s2_3.gif" alt=""></div>';
 	break;
     case 3:
-	content='<link rel="stylesheet" type="text/css" href="slide3/slide3.css" media="screen"/><div class="s3_1"><img src="slide3/s3_1.png"/></div><div class="s3_2"><img src="slide3/s3_2.png"/></div><div class="s3_3"><img src="slide3/s3_3.png"/></div><div class="s3_pop1" onclick="s3_pop1()"></div><div class="s3_c1ose1" onclick="s3_close1()"></div>';
+	content='<link rel="stylesheet" type="text/css" href="slide2/slide2.css" media="screen"/><div class="background"><img src="slide3/s3_1.jpg" width="1024" height="768" alt=""></div><div class="slider"><img src="slide3/s3_2.png" alt=""></div><div class="logo"><img src="slide2/s2_3.gif" alt=""></div>';
 	break;
-	case 4:
-	content='<link rel="stylesheet" type="text/css" href="slide4/slide4.css" media="screen"/><div class="s4_1"><img src="slide4/s4_1.png"/></div><div class="s4_2"><img src="slide4/s4_2.png"/></div><div class="s4_3"><img src="slide4/s4_3.png"/></div><div class="s4_4"><img src="slide4/s4_4.png"/></div><div class="s4_5"><img src="slide4/s4_5.png"/></div><div class="s4_6"><img src="slide4/s4_6.png"/></div><div class="s4_7"><img src="slide4/s4_7.png"/></div><div class="s4_pop1" onclick="s4_pop1()"></div><div class="s4_c1ose1" onclick="s4_close1()"></div>';
+    case 4:
+	content='<link rel="stylesheet" type="text/css" href="slide2/slide2.css" media="screen"/><div class="background"><img src="slide4/s4_1.jpg" width="1024" height="768" alt=""></div><div class="slider"><img src="slide4/s4_2.png" alt=""></div><div class="logo"><img src="slide2/s2_3.gif" alt=""></div>';
 	break;
-	case 5:
-	content='<link rel="stylesheet" type="text/css" href="slide5/slide5.css" media="screen"/><div class="s5_1"><img src="slide5/s5_1.png"/></div><div class="s5_2"><img src="slide5/s5_2.png"/></div><div class="s5_3"><img src="slide5/s5_3.png"/></div><div class="s5_4"><img src="slide5/s5_4.png"/></div><div class="s5_5"><img src="slide5/s5_5.png"/></div><div class="s5_6"><img src="slide5/s5_6.png"/></div><div class="s5_7"><img src="slide5/s5_7.png"/></div><div class="s5_pop1" onclick="s5_pop1()"></div><div class="s5_c1ose1" onclick="s5_close1()"></div>';
+    case 5:
+	content='<link rel="stylesheet" type="text/css" href="slide2/slide2.css" media="screen"/><div class="background"><img src="slide5/s5_1.jpg" width="1024" height="768" alt=""></div><div class="slider"><img src="slide5/s5_2.png" alt=""></div><div class="logo"><img src="slide2/s2_3.gif" alt=""></div>';
 	break;
 	case 6:
-	content='<link rel="stylesheet" type="text/css" href="slide6/slide6.css" media="screen"/><div class="s6_1"><img src="slide6/s6_1.png"/></div><div class="s6_2"><img src="slide6/s6_2.png"/></div><div class="s6_3"><img src="slide6/s6_3.png"/></div><div class="s6_4"><img src="slide6/s6_4.png"/></div><div class="s6_5"><img src="slide6/s6_5.png"/></div><div class="s6_6"><img src="slide6/s6_6.png"/></div><div class="s6_pop1" onclick="s6_pop1()"></div><div class="s6_c1ose1" onclick="s6_close1()"></div>';
+	content='<link rel="stylesheet" type="text/css" href="slide2/slide2.css" media="screen"/><div class="background"><img src="slide6/s6_1.jpg" width="1024" height="768" alt=""></div><div class="slider"><img src="slide6/s6_2.png" alt=""></div><div class="logo"><img src="slide2/s2_3.gif" alt=""></div>';
 	break;
 	case 7:
-	content='<link rel="stylesheet" type="text/css" href="slide7/slide7.css" media="screen"/><div class="s7_1"><img src="slide7/s7_1.png"/></div><div class="s7_2"><img src="slide7/s7_2.png"/></div><div class="s7_3"><img src="slide7/s7_3.png"/></div><div class="s7_4"><img src="slide7/s7_4.png"/></div><div class="s7_pop1" onclick="s7_pop1()"></div><div class="s7_c1ose1" onclick="s7_close1()"></div>';
+	content='<link rel="stylesheet" type="text/css" href="slide2/slide2.css" media="screen"/><div class="background"><img src="slide7/s7_1.jpg" width="1024" height="768" alt=""></div><div class="slider"><img src="slide7/s7_2.png" alt=""></div><div class="logo"><img src="slide2/s2_3.gif" alt=""></div>';
 	break;
 	case 8:
-	content='<link rel="stylesheet" type="text/css" href="slide8/slide8.css" media="screen"/><div class="s8_1"><img src="slide8/s8_1.png"/></div>';
+	content='<link rel="stylesheet" type="text/css" href="slide2/slide2.css" media="screen"/><div class="background"><img src="slide8/s8_1.jpg" width="1024" height="768" alt=""></div><div class="slider"><img src="slide8/s8_2.png" alt=""></div><div class="logo"><img src="slide2/s2_3.gif" alt=""></div>';
 	break;
 }
 
@@ -428,9 +428,10 @@ function open_page(url,page_id){
 	if (typeof(localStorage.getItem("currentslide"))!='undefined'){
 		//to checked previous slide has god end time...
 		var slideid=localStorage.getItem("currentslide");
-		toCaptureTime(slideid);	
+		toCaptureTime(slideid);
+		
 	}
-
+	
 	// toCaptureTime(page_id);
 	 localStorage.setItem("currentslide",page_id);
 	 currentContentNSlide = currentContentId+"_"+contentName+"_"+page_id;
@@ -469,18 +470,22 @@ function open_page(url,page_id){
 	  checkClickThrough();
 	}
 
-function checkClickThrough(){
+	function checkClickThrough(){
 	var currentslide=localStorage.getItem("currentslide");
 	//alert(currentslide);
 	document.getElementById("click_through").innerHTML='';
 
-	if(currentslide == 1) {
-		document.getElementById("click_through").innerHTML='';
-	}
-}
+	if(currentslide == 1){
+	document.getElementById("click_through").innerHTML='';
+		}
+    if(currentslide == 2){
+	document.getElementById("click_through").innerHTML='';
+		}
 
-	function checkBtns(refNum) {
-		switch(refNum) {
+	}
+
+	function checkBtns(refNum){
+		switch(refNum){
 			case 1:
 			open_page('',1);
             break;
@@ -543,55 +548,3 @@ $(document).ready(function(){
 		$('.touchbtn').css("display","block");
 	})
 })
-
-/*--------------------- animation javascript -----------------------*/
-
-function s1_pop1() {
-	$('.s1_3').css("display","block");
-	$('.s1_c1ose1').css("display","block");
-}
-
-function s1_close1() {
-	$('.s1_3').css("display","none");
-	$('.s1_c1ose1').css("display","none");
-}
-
-function s2_pop1() {
-	$('.s2_3').css("display","block");
-	$('.s2_c1ose1').css("display","block");
-}
-
-function s2_close1() {
-	$('.s2_3').css("display","none");
-	$('.s2_c1ose1').css("display","none");
-}
-
-function s3_pop1() {
-	$('.s3_3').css("display","block");
-	$('.s3_c1ose1').css("display","block");
-}
-
-function s3_close1() {
-	$('.s3_3').css("display","none");
-	$('.s3_c1ose1').css("display","none");
-}
-
-function s4_pop1() {
-	$('.s4_3').css("display","block");
-	$('.s4_c1ose1').css("display","block");
-}
-
-function s4_close1() {
-	$('.s4_3').css("display","none");
-	$('.s4_c1ose1').css("display","none");
-}
-
-function s5_pop1() {
-	$('.s5_3').css("display","block");
-	$('.s5_c1ose1').css("display","block");
-}
-
-function s5_close1() {
-	$('.s5_3').css("display","none");
-	$('.s5_c1ose1').css("display","none");
-}
